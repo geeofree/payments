161e59457257589230cdc5029ffa5a4cf6ec3c43
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2023-05-10 19:02:45 +0800

    chore: use a non-expiring session for factory classes
    The factory base class should use a session that doesn't auto-expire on
    commit so that the orm objects don't have to be manually refreshed;
    making unnecessary queries.

--- factories/base.py
+++ factories/base.py
@@ -1,32 +1,28 @@
 from sqlalchemy import insert
+from sqlalchemy.orm import Session
 from database import db
 from faker import Faker
 
 _fake = Faker()
+_factory_session = Session(db.engine, expire_on_commit=False)
 
 class Factory:
-    def __init__(self):
-        self.fake = Faker()
-
-
     def create(self, **kwargs):
-        with db.session as session:
+        with _factory_session as session:
             attributes = { **self.define_attributes(), **kwargs }
             new_instance = self.model(**attributes)
             session.add(new_instance)
             session.commit()
-            session.refresh(new_instance)
             return new_instance
 
 
     def create_many(self, count):
-        with db.session as session:
+        with _factory_session as session:
             results = session.scalars(
                 insert(self.model).returning(self.model),
                 [self.define_attributes() for _ in range(count)]
             ).all()
             session.commit()
-            [session.refresh(result) for result in results]
             return results
 
 
